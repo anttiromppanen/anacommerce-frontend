c27ec3db88d3f5976788e46b63965ebeda29cac0
Antti Romppanen <romppanen.antti@gmail.com>
2023-04-21 03:35:30 +0300

    added searchbar to nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,58 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-
+import {
+  AppBar, Box, Toolbar, Typography, Button, IconButton, Grid, useMediaQuery,
+} from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 import Sidebar from './Sidebar';
+import Searchbar from './Searchbar';
 
 function Navbar() {
+  const breakpoint400px = useMediaQuery('(max-width: 400px)');
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static" elevation={0}>
-        <Toolbar sx={{ position: 'relative' }}>
-          <Sidebar />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Ana-commerce
-          </Typography>
-          <Button color="inherit" size="large">Login</Button>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="shopping-cart"
-            sx={{ ml: 0.5 }}
+        <Toolbar component={Grid} container sx={{ position: 'relative' }}>
+          <Grid
+            item
+            container
+            alignItems="center"
+            xs={8}
+            md={4}
           >
-            <ShoppingCartIcon />
-          </IconButton>
+            <Sidebar />
+            <Typography variant="h6" component="div">
+              Ana-commerce
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            order={{ xs: 3, md: 2 }}
+            mb={{ xs: 2, md: 0 }}
+          >
+            <Searchbar />
+          </Grid>
+          <Grid
+            item
+            container
+            justifyContent="flex-end"
+            xs={4}
+            md={4}
+            order={{ xs: 2, md: 3 }}
+          >
+            <Button color="inherit" size="large" sx={{ display: breakpoint400px ? 'none' : 'inline-flex' }}>Login</Button>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="shopping-cart"
+              sx={{ ml: 0.5 }}
+            >
+              <ShoppingCartIcon />
+            </IconButton>
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
