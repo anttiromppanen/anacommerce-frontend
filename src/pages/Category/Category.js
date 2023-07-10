3d6c305ab77ffe9a9aca14116022ebdb3a9dabb2
Antti Romppanen <romppanen.antti@gmail.com>
2023-07-10 14:27:35 +0300

    refactor

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -3,53 +3,21 @@ import { useParams } from 'react-router-dom';
 
 import {
   Box,
-  Card, CardContent, CardMedia, Grid, Typography, Stack, useMediaQuery, styled, Container, Divider,
+  Grid,
+  Typography,
+  useMediaQuery,
+  Container,
 } from '@mui/material';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useTheme } from '@mui/material/styles';
 
 import { useFetchSubcategories } from '../../hooks/useGetSubcategories';
-
-const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
-  height: 150,
-  width: 150,
-
-  [theme.breakpoints.only('xs')]: {
-    height: 50,
-    width: 50,
-  },
-}));
-
-const StyledCardContainer = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(4),
-  backgroundColor: theme.palette.grey[100],
-  width: 'auto',
-
-  [theme.breakpoints.only('xs')]: {
-    backgroundColor: 'inherit',
-    padding: 0,
-    width: '100%',
-  },
-}));
-
-const StyledCardContent = styled(CardContent)(({ theme }) => ({
-  textAlign: 'center',
-  [theme.breakpoints.only('xs')]: {
-    padding: 0,
-    paddingLeft: theme.spacing(1),
-    '&:last-child': {
-      paddingBottom: 0,
-    },
-  },
-}));
+import SubcategoryGrid from './SubcategoryGrid';
 
 function Category() {
   const { category } = useParams();
   const { isLoading, data } = useFetchSubcategories(category);
   const theme = useTheme();
   const mobileViewIsActive = useMediaQuery(theme.breakpoints.only('xs'));
-
-  if (isLoading) return <Typography>Loading...</Typography>;
 
   return (
     <Container maxWidth="xl">
@@ -65,51 +33,7 @@ function Category() {
         mt={theme.spacing(0.5)}
         spacing={mobileViewIsActive ? 1 : 5}
       >
-        {
-          data.map((subcategory) => (
-            <Grid
-              item
-              container
-              justifyContent="center"
-              xs={12}
-              sm={4}
-              md={3}
-              lg={2}
-              key={subcategory.subcategoryName}
-            >
-              <StyledCardContainer>
-                <Card
-                  sx={{ width: mobileViewIsActive ? '100%' : 150 }}
-                  elevation={0}
-                  component={Stack}
-                  direction={mobileViewIsActive ? 'row' : 'column'}
-                  alignItems="center"
-                >
-                  <StyledCardMedia
-                    image={subcategory.subcategoryIcon}
-                    title="Subcategory icon"
-                  />
-                  <StyledCardContent
-                    component={Grid}
-                    container
-                    justifyContent={mobileViewIsActive ? 'space-between' : 'center'}
-                    alignItems="center"
-                  >
-                    <Typography variant="h5">
-                      {subcategory.subcategoryName}
-                    </Typography>
-                    {
-                      mobileViewIsActive && <ArrowForwardIosIcon />
-                    }
-                  </StyledCardContent>
-                </Card>
-                {
-                  mobileViewIsActive && <Divider sx={{ pt: 1 }} />
-                }
-              </StyledCardContainer>
-            </Grid>
-          ))
-        }
+        <SubcategoryGrid data={data} isLoading={isLoading} />
       </Grid>
     </Container>
   );
---
+++ src/pages/Category/SubcategoryGrid.js
@@ -0,0 +1,104 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+  styled,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  height: 150,
+  width: 150,
+
+  [theme.breakpoints.only('xs')]: {
+    height: 50,
+    width: 50,
+  },
+}));
+
+const StyledCardContent = styled(CardContent)(({ theme }) => ({
+  textAlign: 'center',
+  [theme.breakpoints.only('xs')]: {
+    padding: 0,
+    paddingLeft: theme.spacing(1),
+    '&:last-child': {
+      paddingBottom: 0,
+    },
+  },
+}));
+
+const StyledCardContainer = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(4),
+  backgroundColor: theme.palette.grey[100],
+  width: 'auto',
+
+  [theme.breakpoints.only('xs')]: {
+    backgroundColor: 'inherit',
+    padding: 0,
+    width: '100%',
+  },
+}));
+
+function SubcategoryGrid({ isLoading, data }) {
+  const theme = useTheme();
+  const mobileViewIsActive = useMediaQuery(theme.breakpoints.only('xs'));
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+
+  return (
+    data.map((subcategory) => (
+      <Grid
+        item
+        container
+        justifyContent="center"
+        xs={12}
+        sm={4}
+        md={3}
+        lg={2}
+        key={subcategory.subcategoryName}
+      >
+        <StyledCardContainer>
+          <Card
+            sx={{ width: mobileViewIsActive ? '100%' : 150 }}
+            elevation={0}
+            component={Stack}
+            direction={mobileViewIsActive ? 'row' : 'column'}
+            alignItems="center"
+          >
+            <StyledCardMedia
+              image={subcategory.subcategoryIcon}
+              title="Subcategory icon"
+            />
+            <StyledCardContent
+              component={Grid}
+              container
+              justifyContent={mobileViewIsActive ? 'space-between' : 'center'}
+              alignItems="center"
+            >
+              <Typography variant="h5">
+                {subcategory.subcategoryName}
+              </Typography>
+              {
+                mobileViewIsActive && <ArrowForwardIosIcon />
+              }
+            </StyledCardContent>
+          </Card>
+          {
+            mobileViewIsActive && <Divider sx={{ pt: 1 }} />
+          }
+        </StyledCardContainer>
+      </Grid>
+    )));
+}
+
+export default SubcategoryGrid;
